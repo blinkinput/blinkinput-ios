ebb0e452336ced9e496f64131be0136fb6f71463
Jurica Cerovec <jurica.cerovec@microblink.com>
2015-07-28 23:47:04 +0200

    Added travis scripts

---
+++ travis/script.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+
+xctool -project Samples/BlinkOCR-sample/BlinkOCR-sample.xcodeproj \
+	-scheme BlinkOCR-sample \
+	-configuration Debug \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
+
+xctool -project Samples/BlinkOCR-sample/BlinkOCR-sample.xcodeproj \
+	-scheme BlinkOCR-sample \
+	-configuration Release \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
+
+xctool -project Samples/NoCamera-sample/NoCamera-sample.xcodeproj \
+	-scheme NoCamera-sample \
+	-configuration Debug \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
+
+xctool -project Samples/NoCamera-sample/NoCamera-sample.xcodeproj \
+	-scheme NoCamera-sample \
+	-configuration Release \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
+
+xctool -project Samples/FormScanning-sample/FormScanning-sample.xcodeproj \
+	-scheme FormScanning-sample \
+	-configuration Debug \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
+
+xctool -project Samples/FormScanning-sample/FormScanning-sample.xcodeproj \
+	-scheme FormScanning-sample \
+	-configuration Release \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
