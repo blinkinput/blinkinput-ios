a6d57cb67d451ee21ff5fc41b7c36a609f7d3ef5
Jurica Cerovec <jurica.cerovec@microblink.com>
2015-07-29 23:58:01 +0200

    Simplified NoCamera sample app

--- Samples/NoCamera-sample/NoCamera-sample/ViewController.m
+++ Samples/NoCamera-sample/NoCamera-sample/ViewController.m
@@ -13,11 +13,11 @@
 
 @interface ViewController () <PPScanDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
+@property (nonatomic, strong) NSString *rawOcrParserId;
+
+@property (nonatomic, strong) NSString *priceParserId;
+
 @property (nonatomic, strong) PPCoordinator *coordinator;
-
-@property (nonatomic, strong) NSString* rawOcrParserId;
-
-@property (nonatomic, strong) NSString* priceParserId;
 
 @end
 
@@ -25,15 +25,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 
     self.rawOcrParserId = @"Raw ocr";
     self.priceParserId = @"Price";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ * Method allocates and initializes the Scanning coordinator object.
+ * Coordinator is initialized with settings for scanning
+ *
+ *  @param error Error object, if scanning isn't supported
+ *
+ *  @return initialized coordinator
+ */
+- (PPCoordinator *)coordinatorWithError:(NSError**)error {
+
+    /** 0. Check if scanning is supported */
+
+    if ([PPCoordinator isScanningUnsupported:error]) {
+        return nil;
+    }
+
+
+    /** 1. Initialize the Scanning settings */
+
+    // Initialize the scanner settings object. This initialize settings with all default values.
+    PPSettings *settings = [[PPSettings alloc] init];
+
+
+    /** 2. Setup the license key */
+
+    // Visit www.microblink.com to get the license key for your app
+    settings.licenseSettings.licenseKey = @"W524FSU4-QYAUEKG4-X2IAMBQG-AYDAMBQG-AYDAMBQG-AYDAMBQG-AYDAMBQG-7C3KC3SB";
+
+
+    /**
+     * 3. Set up what is being scanned. See detailed guides for specific use cases.
+     * Here's an example for initializing raw OCR scanning.
+     */
+
+    // To specify we want to perform OCR recognition, initialize the OCR recognizer settings
+    PPOcrRecognizerSettings *ocrRecognizerSettings = [[PPOcrRecognizerSettings alloc] init];
+
+    // We want raw OCR parsing
+    [ocrRecognizerSettings addOcrParser:[self priceOcrParserFactory] name:self.priceParserId];
+
+    // We want to parse prices from raw OCR result as well
+    [ocrRecognizerSettings addOcrParser:[self rawOcrParserFactory] name:self.rawOcrParserId];
+
+    // Add the recognizer setting to a list of used recognizer
+    [settings.scanSettings addRecognizerSettings:ocrRecognizerSettings];
+
+
+    /** 4. Initialize the Scanning Coordinator object */
+
+    PPCoordinator *coordinator = [[PPCoordinator alloc] initWithSettings:settings];
+    
+    return coordinator;
 }
 
 - (PPPriceOcrParserFactory *)priceOcrParserFactory {
@@ -54,7 +102,7 @@
 
     options.minimalLineHeight = 20.0f; // minimal line height shall be more than 20 pixels
     options.maximalLineHeight = 80.f; // maximal line height shall be less than 80 pixels
-    options.maxCharsExpected = 600; // there will be no more than 600 characters on the image
+    options.maxCharsExpected = 1200; // there will be no more than 1200 characters on the image
     options.colorDropoutEnabled = NO; // don't use color dropot method as we assume only black/white scenes will be scanned
 
     // initialize new char whitelist
@@ -91,47 +139,22 @@
 }
 
 - (IBAction)takePhoto:(id)sender {
-    NSLog(@"Take photo!");
-
-    // Check if scanning is supported
+    /** Instantiate the scanning coordinator */
     NSError *error;
-    if ([PPCoordinator isScanningUnsupported:&error]) {
+
+    self.coordinator = [self coordinatorWithError:&error];
+
+    /** If scanning isn't supported, present an error */
+    if (self.coordinator == nil) {
         NSString *messageString = [error localizedDescription];
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning"
-                                                        message:messageString
-                                                       delegate:nil
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil, nil];
-        [alert show];
+        [[[UIAlertView alloc] initWithTitle:@"Warning"
+                                    message:messageString
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil, nil] show];
+
         return;
     }
-
-    /** 1. Initialize the Scanning settings */
-
-    // Initialize the scanner settings object. This initialize settings with all default values.
-    PPSettings *settings = [[PPSettings alloc] init];
-
-
-    /** 2. Setup the license key */
-    settings.licenseSettings.licenseKey = @"W524FSU4-QYAUEKG4-X2IAMBQG-AYDAMBQG-AYDAMBQG-AYDAMBQG-AYDAMBQG-7C3KC3SB";
-
-
-    /** 3. Set up what is being scanned. See detailed guides for specific use cases. Here's an example for initializing raw and price OCR scanning. */
-
-    // To specify we want to perform OCR recognition, initialize the OCR recognizer settings
-    PPOcrRecognizerSettings *ocrRecognizerSettings = [[PPOcrRecognizerSettings alloc] init];
-
-    /** 3.a - We want to parse prices from raw OCR result as well */
-    [ocrRecognizerSettings addOcrParser:[self priceOcrParserFactory] name:self.priceParserId];
-
-    /** 3.b - We want raw OCR parsing, but with custom OCR settings */
-    [ocrRecognizerSettings addOcrParser:[self rawOcrParserFactory] name:self.rawOcrParserId];
-
-    /** 3.c Add the recognizer setting to a list of used recognizer */
-    [settings.scanSettings addRecognizerSettings:ocrRecognizerSettings];
-
-    // get coordinator
-    self.coordinator = [[PPCoordinator alloc] initWithSettings:settings];
 
     UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
 
@@ -146,18 +169,12 @@
     cameraUI.allowsEditing = NO;
 
     // Shows default camera control overlay over camera preview.
-    // TODO: set this to NO and provide custom overlay
     cameraUI.showsCameraControls = YES;
 
     // set delegate
     cameraUI.delegate = self;
 
-    [self.coordinator initializeRecognizers];
-
     // Show view
-    // in iOS7 (as of DP6) this shows a bugged status bar (see https://devforums.apple.com/message/861462#861462)
-    // TODO: iOS 6 should be tested
-    // iOS5 works OK, just like Facebook app
     [self presentViewController:cameraUI animated:YES completion:nil];
 }
 
@@ -170,12 +187,11 @@
     if (CFStringCompare((CFStringRef) mediaType, kUTTypeImage, 0) == kCFCompareEqualTo) {
         UIImage *originalImage = (UIImage *)[info objectForKey: UIImagePickerControllerOriginalImage];
 
-
         [self.coordinator processImage:originalImage
                         scanningRegion:CGRectMake(0.0, 0.0, 1.0, 1.0)
                               delegate:self];
     }
-
+    
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -200,6 +216,9 @@
     // Here you process scanning results. Scanning results are given in the array of PPRecognizerResult objects.
     // Perform your logic here
 
+    NSString *title = @"No result";
+    NSString *message = nil;
+
     for (PPRecognizerResult *result in results) {
         if ([result isKindOfClass:[PPOcrRecognizerResult class]]) {
             PPOcrRecognizerResult* ocrRecognizerResult = (PPOcrRecognizerResult*)result;
@@ -211,15 +230,18 @@
             PPOcrLayout* ocrLayout = [ocrRecognizerResult ocrLayout];
             NSLog(@"Dimensions of ocrLayout are %@", NSStringFromCGRect([ocrLayout box]));
 
-            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"OCR result"
-                                                                message:[ocrRecognizerResult parsedResultForName:self.rawOcrParserId]
-                                                               delegate:nil
-                                                      cancelButtonTitle:@"OK"
-                                                      otherButtonTitles: nil];
-
-            [alertView show];
+            title = @"OCR result";
+            message = [ocrRecognizerResult parsedResultForName:self.rawOcrParserId];
         }
     };
+
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles: nil];
+
+    [alertView show];
 }
 
 @end
