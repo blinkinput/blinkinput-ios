2280bbd4db7e0b2becc5c0cb7c4cf537825399cd
Jurica Cerovec <jurica.cerovec@microblink.com>
2015-09-09 02:10:30 +0200

    Updated travis script to include pdf417-sample app

--- travis/script.sh
+++ travis/script.sh
@@ -42,3 +42,17 @@ xctool -project Samples/FormScanning-sample/FormScanning-sample.xcodeproj \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
+
+xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+	-scheme pdf417-sample \
+	-configuration Debug \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
+
+xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+	-scheme pdf417-sample \
+	-configuration Release \
+	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
+ 	clean build
