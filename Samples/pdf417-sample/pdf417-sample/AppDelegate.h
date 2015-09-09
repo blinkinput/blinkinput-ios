820e8926931403987d3f4b99cdc4f07f40df9a73
Jurica Cerovec <jurica.cerovec@microblink.com>
2015-09-09 02:08:27 +0200

    Added pdf417-sample for barcode scanning

---
+++ Samples/pdf417-sample/pdf417-sample/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  pdf417-sample
+//
+//  Created by Jura on 16/07/15.
+//  Copyright (c) 2015 MicroBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Samples/pdf417-sample/pdf417-sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pdf417-sample
+//
+//  Created by Jura on 16/07/15.
+//  Copyright (c) 2015 MicroBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
