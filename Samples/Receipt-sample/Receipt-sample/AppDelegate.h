4273a396d4e3c6ae2cb2ac66d8c8447b946740ba
Jurica Cerovec <jurica.cerovec@microblink.com>
2015-10-21 18:22:55 +0200

    Added receipt scanning demo

---
+++ Samples/Receipt-sample/Receipt-sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Receipt-sample
+//
+//  Created by Jura on 21/10/15.
+//  Copyright © 2015 MicroBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Samples/Receipt-sample/Receipt-sample/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Receipt-sample
+//
+//  Created by Jura on 21/10/15.
+//  Copyright © 2015 MicroBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)didTapScan:(id)sender;
+
+@end
+
