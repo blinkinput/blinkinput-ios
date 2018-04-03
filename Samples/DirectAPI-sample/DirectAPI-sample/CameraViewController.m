103df6eeb6cc52aec4c9ae17757cf8b31faab03b
juraskrlec <jura.skrlec@microblink.com>
2018-04-03 16:38:05 +0200

    Updated Direct-API to new API

--- Samples/DirectAPI-sample/DirectAPI-sample/CameraViewController.m
+++ Samples/DirectAPI-sample/DirectAPI-sample/CameraViewController.m
@@ -11,13 +11,16 @@
 #import <AVFoundation/AVFoundation.h>
 #import <MicroBlink/MicroBlink.h>
 
-@interface CameraViewController () <AVCaptureAudioDataOutputSampleBufferDelegate, AVCaptureVideoDataOutputSampleBufferDelegate, PPCoordinatorDelegate>
+@interface CameraViewController () <AVCaptureAudioDataOutputSampleBufferDelegate, AVCaptureVideoDataOutputSampleBufferDelegate, MBScanningRecognizerRunnerDelegate>
 
 @property (weak, nonatomic) IBOutlet CameraView *cameraView;
 
 @property (nonatomic, strong) AVCaptureSession *captureSession;
 
-@property (nonatomic, strong) PPCoordinator *coordinator;
+@property (nonatomic, strong) MBRecognizerRunner *recognizerRunner;
+@property (nonatomic, strong) MBPdf417Recognizer *pdf417Recognizer;
+
+@property (nonatomic) BOOL pauseRecognition;
 
 @end
 
@@ -27,11 +30,14 @@ static NSString *rawOcrParserId = @"Raw ocr";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Valid until: 2018-04-29
+    [[MBMicroblinkSDK sharedInstance] setLicenseResource:@"license" withExtension:@"txt" inSubdirectory:@"License" forBundle:[NSBundle mainBundle]];
 }
 
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
     [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-
+    
     // Note that the app delegate controls the device orientation notifications required to use the device orientation.
     UIDeviceOrientation deviceOrientation = [UIDevice currentDevice].orientation;
     if (UIDeviceOrientationIsPortrait(deviceOrientation) || UIDeviceOrientationIsLandscape(deviceOrientation)) {
@@ -54,32 +60,32 @@ static NSString *rawOcrParserId = @"Raw ocr";
                                              selector:@selector(appplicationWillResignActive:)
                                                  name:UIApplicationWillResignActiveNotification
                                                object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(appplicationWillEnterForeground:)
                                                  name:UIApplicationWillEnterForegroundNotification
                                                object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(applicationDidEnterBackgroundNotification:)
                                                  name:UIApplicationDidEnterBackgroundNotification
                                                object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(applicationWillTerminateNotification:)
                                                  name:UIApplicationWillTerminateNotification
                                                object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(captureSessionDidStartRunningNotification:)
                                                  name:AVCaptureSessionDidStartRunningNotification
                                                object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(captureSessionDidStopRunningNotification:)
                                                  name:AVCaptureSessionDidStopRunningNotification
                                                object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(captureSessionRuntimeErrorNotification:)
                                                  name:AVCaptureSessionRuntimeErrorNotification
@@ -125,7 +131,7 @@ static NSString *rawOcrParserId = @"Raw ocr";
 
 - (void)captureSessionDidStartRunningNotification:(NSNotification*)note {
     NSLog(@"captureSessionDidStartRunningNotification!");
-
+    
     [UIView animateWithDuration:0.3 animations:^{
         self.cameraPausedLabel.alpha = 0.0;
     }];
@@ -133,7 +139,7 @@ static NSString *rawOcrParserId = @"Raw ocr";
 
 - (void)captureSessionDidStopRunningNotification:(NSNotification*)note {
     NSLog(@"captureSessionDidStopRunningNotification!");
-
+    
     [UIView animateWithDuration:0.3 animations:^{
         self.cameraPausedLabel.alpha = 1.0;
     }];
@@ -145,16 +151,16 @@ static NSString *rawOcrParserId = @"Raw ocr";
 
 
 - (void)startCaptureSession {
-
+    
     // Create session
     self.captureSession = [[AVCaptureSession alloc] init];
     self.captureSession.sessionPreset = AVCaptureSessionPresetHigh;
-
+    
     // Init the device inputs
     AVCaptureDeviceInput *videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:[self cameraWithPosition:AVCaptureDevicePositionBack]
                                                                               error:nil];
     [self.captureSession addInput:videoInput];
-
+    
     // setup video data output
     AVCaptureVideoDataOutput *videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
     [videoDataOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA] forKey:(id)kCVPixelBufferPixelFormatTypeKey]];
@@ -162,12 +168,22 @@ static NSString *rawOcrParserId = @"Raw ocr";
     
     dispatch_queue_t queue = dispatch_queue_create("myQueue", NULL);
     [videoDataOutput setSampleBufferDelegate:self queue:queue];
-
+    
     // Setup the preview view.
     self.cameraView.session = self.captureSession;
     
-    [self createCoordinator];
-
+    NSMutableArray<MBRecognizer *> *recognizers = [[NSMutableArray alloc] init];
+    
+    self.pdf417Recognizer = [[MBPdf417Recognizer alloc] init];
+    
+    [recognizers addObject:self.pdf417Recognizer];
+    
+    MBSettings* settings = [[MBSettings alloc] init];
+    settings.uiSettings.recognizerCollection = [[MBRecognizerCollection alloc] initWithRecognizers:recognizers];
+    
+    self.recognizerRunner = [[MBRecognizerRunner alloc] initWithSettings:settings];
+    self.recognizerRunner.scanningRecognizerRunnerDelegate = self;
+    
     [self.captureSession startRunning];
 }
 
@@ -188,67 +204,40 @@ static NSString *rawOcrParserId = @"Raw ocr";
 }
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
-    PPImage *image = [PPImage imageWithCmSampleBuffer:sampleBuffer];
-    image.orientation = PPProcessingOrientationLeft;
-    [self.coordinator processImage:image];
+    MBImage *image = [MBImage imageWithCmSampleBuffer:sampleBuffer];
+    image.orientation = MBProcessingOrientationLeft;
+    if (!self.pauseRecognition) {
+        [self.recognizerRunner processImage:image];
+    }
 }
 
-- (void)createCoordinator {
-    
-    
-    
-    /** 1. Initialize the Scanning settings */
-    
-    // Initialize the scanner settings object. This initialize settings with all default values.
-    PPSettings *settings = [[PPSettings alloc] init];
-    
-    
-    /** 2. Setup the license key */
-    
-    // Visit www.microblink.com to get the license key for your app
-    // // Valid until 2017-12-21
-    settings.licenseSettings.licenseKey = @"VVDNJH5C-2NATW4P4-7TYCJVR4-EEX53IOG-VL7U3D6C-FP5ITGVQ-6SW4L45J-JDDQ4BGZ";
-    
-    
-    /**
-     * 3. Set up what is being scanned. See detailed guides for specific use cases.
-     * Here's an example for initializing raw OCR scanning.
-     */
-    
-    // To specify we want to perform OCR recognition, initialize the OCR recognizer settings
-    PPBlinkInputRecognizerSettings *ocrRecognizerSettings = [[PPBlinkInputRecognizerSettings alloc] init];
-    
-    // We want raw OCR parsing
-    [ocrRecognizerSettings addOcrParser:[[PPRawOcrParserFactory alloc] init] name:rawOcrParserId];
-    
-    // Add the recognizer setting to a list of used recognizer
-    [settings.scanSettings addRecognizerSettings:ocrRecognizerSettings];
-    
-    /** 4. Initialize the Scanning Coordinator object */
-    
-    PPCoordinator *coordinator = [[PPCoordinator alloc] initWithSettings:settings delegate:self];
-    
-    self.coordinator = coordinator;
-}
-
-- (void)coordinator:(PPCoordinator *)coordinator didOutputResults:(NSArray<PPRecognizerResult *> *)results {
-    // Here you process scanning results. Scanning results are given in the array of PPRecognizerResult objects.
-    
-    
-    // Collect data from the result
-    for (PPRecognizerResult* result in results) {
-        
-        if ([result isKindOfClass:[PPBlinkInputRecognizerResult class]]) {
-            PPBlinkInputRecognizerResult* ocrRecognizerResult = (PPBlinkInputRecognizerResult*)result;
+- (void)recognizerRunnerDidFinish:(MBRecognizerRunner *)recognizerRunner state:(MBRecognizerResultState)state {
+    self.pauseRecognition = YES;
+    if (self.pdf417Recognizer.result.resultState == MBRecognizerResultStateValid) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSString *title = @"PDF417";
             
-            NSLog(@"OCR results are:");
-            NSLog(@"Raw ocr: %@", [ocrRecognizerResult parsedResultForName:rawOcrParserId]);
+            // Save the string representation of the code
+            NSString *message = [self.pdf417Recognizer.result stringData];
             
-            PPOcrLayout* ocrLayout = [ocrRecognizerResult ocrLayout];
-            NSLog(@"Dimensions of ocrLayout are %@", NSStringFromCGRect([ocrLayout box]));
-        }
-    };
-    
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title
+                                                                                     message:message
+                                                                              preferredStyle:UIAlertControllerStyleAlert];
+            
+            UIAlertAction* okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                               style:UIAlertActionStyleDefault
+                                                             handler:^(UIAlertAction * _Nonnull action) {
+                                                                 [self dismissViewControllerAnimated:YES completion:nil];
+                                                             }];
+            
+            [alertController addAction:okAction];
+            
+            [self presentViewController:alertController animated:YES completion:nil];
+        });
+    } else {
+        self.pauseRecognition = NO;
+    }
 }
 
 @end
+
--- Samples/DirectAPI-sample/DirectAPI-sample/RootViewController.m
+++ Samples/DirectAPI-sample/DirectAPI-sample/RootViewController.m
@@ -11,9 +11,10 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 #import <MicroBlink/MicroBlink.h>
 
-@interface RootViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, PPCoordinatorDelegate>
+@interface RootViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, MBScanningRecognizerRunnerDelegate>
 
-@property (nonatomic) PPCoordinator *coordinator;
+@property (nonatomic, strong) MBRecognizerRunner *recognizerRunner;
+@property (nonatomic, strong) MBPdf417Recognizer *pdf417Recognizer;
 
 @end
 
@@ -21,103 +22,94 @@
 
 static NSString* rawOcrParserId = @"RawOcrParser";
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [[MBMicroblinkSDK sharedInstance] setLicenseResource:@"license-direct" withExtension:@"txt" inSubdirectory:@"License" forBundle:[NSBundle mainBundle]];
+    [self setupRecognizerRunner];
+}
+
 - (IBAction)openImagePicker:(id)sender {
-
+    
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
-
+    
     imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
     imagePicker.cameraDevice = UIImagePickerControllerCameraDeviceRear;
-
+    
     // Displays a control that allows the user to choose only photos
     imagePicker.mediaTypes = [[NSArray alloc] initWithObjects: (NSString *)kUTTypeImage, nil];
-
+    
     // Hides the controls for moving & scaling pictures, or for trimming movies.
     imagePicker.allowsEditing = NO;
-
+    
     // Shows default camera control overlay over camera preview.
     imagePicker.showsCameraControls = YES;
-
+    
     // set delegate
     imagePicker.delegate = self;
-
+    
     [self presentViewController:imagePicker animated:YES completion:nil];
 }
 
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
     NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
     
     // Handle a still image capture
     if (CFStringCompare((CFStringRef) mediaType, kUTTypeImage, 0) == kCFCompareEqualTo) {
         UIImage *originalImage = (UIImage *)[info objectForKey: UIImagePickerControllerOriginalImage];
-        
-        if (self.coordinator == nil) {
-            [self createCoordinator];
-        }
-        PPImage *image = [PPImage imageWithUIImage:originalImage];
-        image.cameraFrame = NO;
-        image.orientation = PPProcessingOrientationLeft;
-        [self.coordinator processImage:image];
+        [self processImageRunner: originalImage];
     }
-    
-    [self dismissViewControllerAnimated:YES completion:nil];
+    [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)createCoordinator {
-    
-    
-    
-    /** 1. Initialize the Scanning settings */
-    
-    // Initialize the scanner settings object. This initialize settings with all default values.
-    PPSettings *settings = [[PPSettings alloc] init];
-    
-    
-    /** 2. Setup the license key */
-    
-    // Visit www.microblink.com to get the license key for your app
-    // Valid until 2017-12-21
-    settings.licenseSettings.licenseKey = @"VVDNJH5C-2NATW4P4-7TYCJVR4-EEX53IOG-VL7U3D6C-FP5ITGVQ-6SW4L45J-JDDQ4BGZ";
-    
-    
-    /**
-     * 3. Set up what is being scanned. See detailed guides for specific use cases.
-     * Here's an example for initializing raw OCR scanning.
-     */
-    
-    // To specify we want to perform OCR recognition, initialize the OCR recognizer settings
-    PPBlinkInputRecognizerSettings *ocrRecognizerSettings = [[PPBlinkInputRecognizerSettings alloc] init];
-    
-    // We want raw OCR parsing
-    [ocrRecognizerSettings addOcrParser:[[PPRawOcrParserFactory alloc] init] name:rawOcrParserId];
-    
-    // Add the recognizer setting to a list of used recognizer
-    [settings.scanSettings addRecognizerSettings:ocrRecognizerSettings];    
-    
-    /** 4. Initialize the Scanning Coordinator object */
-    
-    PPCoordinator *coordinator = [[PPCoordinator alloc] initWithSettings:settings delegate:self];
-    
-    self.coordinator = coordinator;
+- (void)recognizerRunnerDidFinish:(MBRecognizerRunner *)recognizerRunner state:(MBRecognizerResultState)state {
+    if (self.pdf417Recognizer.result.resultState == MBRecognizerResultStateValid) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSString *title = @"PDF417";
+            
+            // Save the string representation of the code
+            NSString *message = [self.pdf417Recognizer.result stringData];
+            
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title
+                                                                                     message:message
+                                                                              preferredStyle:UIAlertControllerStyleAlert];
+            
+            UIAlertAction* okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                               style:UIAlertActionStyleDefault
+                                                             handler:^(UIAlertAction * _Nonnull action) {
+                                                                 [self dismissViewControllerAnimated:YES completion:nil];
+                                                             }];
+            
+            [alertController addAction:okAction];
+            
+            [self presentViewController:alertController animated:YES completion:nil];
+        });
+    }
 }
 
-- (void)coordinator:(PPCoordinator *)coordinator didOutputResults:(NSArray<PPRecognizerResult *> *)results {
-    // Here you process scanning results. Scanning results are given in the array of PPRecognizerResult objects.
+- (void)setupRecognizerRunner {
+    NSMutableArray<MBRecognizer *> *recognizers = [[NSMutableArray alloc] init];
     
+    self.pdf417Recognizer = [[MBPdf417Recognizer alloc] init];
     
-    // Collect data from the result
-    for (PPRecognizerResult* result in results) {
-        
-        if ([result isKindOfClass:[PPBlinkInputRecognizerResult class]]) {
-            PPBlinkInputRecognizerResult* ocrRecognizerResult = (PPBlinkInputRecognizerResult*)result;
-            
-            NSLog(@"OCR results are:");
-            NSLog(@"Raw ocr: %@", [ocrRecognizerResult parsedResultForName:rawOcrParserId]);
-            
-            PPOcrLayout* ocrLayout = [ocrRecognizerResult ocrLayout];
-            NSLog(@"Dimensions of ocrLayout are %@", NSStringFromCGRect([ocrLayout box]));
-        }
-    };
+    [recognizers addObject:self.pdf417Recognizer];
     
+    MBSettings* settings = [[MBSettings alloc] init];
+    settings.uiSettings.recognizerCollection = [[MBRecognizerCollection alloc] initWithRecognizers:recognizers];
+    
+    self.recognizerRunner = [[MBRecognizerRunner alloc] initWithSettings:settings];
+    self.recognizerRunner.scanningRecognizerRunnerDelegate = self;
+}
+
+- (void)processImageRunner:(UIImage *)originalImage {
+    MBImage *image = [MBImage imageWithUIImage:originalImage];
+    image.cameraFrame = YES;
+    image.orientation = MBProcessingOrientationLeft;
+    dispatch_queue_t _serialQueue = dispatch_queue_create("com.microblink.DirectAPI-sample", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(_serialQueue, ^{
+        [self.recognizerRunner processImage:image];
+    });
 }
 
 @end
+
