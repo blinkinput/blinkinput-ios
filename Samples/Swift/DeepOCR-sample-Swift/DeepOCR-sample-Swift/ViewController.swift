38796f888cb2d805ecc7b5a8707c4cc497b6a24b
Jurica Cerovec <jurica.cerovec@microblink.com>
2018-05-15 17:41:31 +0200

    DeepOCR sample beautify

--- Samples/Swift/DeepOCR-sample-Swift/DeepOCR-sample-Swift/ViewController.swift
+++ Samples/Swift/DeepOCR-sample-Swift/DeepOCR-sample-Swift/ViewController.swift
@@ -17,16 +17,6 @@ class ViewController: UIViewController {
     
     var customOverlayVC : CustomOverlay?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func didTapScan(_ sender: Any) {
         
         /** Create parsers and groups and recognizer */
