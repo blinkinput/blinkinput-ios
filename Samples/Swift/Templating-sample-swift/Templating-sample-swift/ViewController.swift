dd919214bcc14734b5b3ee7f9d125c3b1b78a432
Jurica Cerovec <jurica.cerovec@microblink.com>
2018-05-16 10:18:49 +0200

    Beautify templating sample

--- Samples/Swift/Templating-sample-swift/Templating-sample-swift/ViewController.swift
+++ Samples/Swift/Templating-sample-swift/Templating-sample-swift/ViewController.swift
@@ -13,17 +13,6 @@ class ViewController: UIViewController {
     
     var croatianIDFrontTemplateRecognizer: MBCroatianIDFrontTemplateRecognizer?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
     @IBAction func scanButtonTapped(_ sender: Any) {
         
         croatianIDFrontTemplateRecognizer = MBCroatianIDFrontTemplateRecognizer()
